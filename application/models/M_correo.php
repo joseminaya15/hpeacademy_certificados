75fe6ca8f28de7d3603391f1614c492a7c0ff6bf
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-04-03 11:26:01 -0500

    cambios

---
+++ application/models/M_correo.php
@@ -0,0 +1,15 @@
+<?php
+
+class M_correo extends  CI_Model{
+    function __construct(){
+        parent::__construct();
+    }
+
+    function getDatosCorreos($name) {
+        $sql = "SELECT *
+                  FROM emails
+                 WHERE Email LIKE ?";
+        $result = $this->db->query($sql, array($name));
+        return $result->result();
+    }
+}
