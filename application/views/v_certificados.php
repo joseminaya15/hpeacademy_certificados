1215fb1e57c570783786d5cc2f570815bfe66114
joseminaya15 <jose.minayac15@gmail.com>
2018-04-03 17:06:39 -0500

    css

--- application/views/v_certificados.php
+++ application/views/v_certificados.php
@@ -51,25 +51,10 @@
                         <?php echo $html ?>
                     </div>
                     <div class="redes">
-                        <a class = "twitter-share-button"
-                          href = "https://twitter.com/intent/tweet?status=<?php echo RUTA_IMG.'2.jpg' ?>"
-                          data-size = "large">
-                          Tweet </a>
-                          </br>
-                          <a class = "linkedin-share-button"
-                          href = "https://www.linkedin.com/shareArticle?mini=true&url=http://www.sap-latam.com/sap_business_one/Prueba&title=Publicación&source=SAP"
-                          data-size = "large">
-                          Linkedin </a>
-                          </br>
-                          <a class = "linkedin-share-button"
-                          href = "http://facebook.com/sharer.php?u=http://www.sap-latam.com/sap_business_one/Prueba"
-                          data-size = "large">
-                          Facebook </a>
-                          </br>
-                          <a class = "linkedin-share-button"
-                          href = "https://plus.google.com/share?url=http://www.sap-latam.com/sap_business_one/Prueba"
-                          data-size = "large">
-                          Google Plus </a>
+                        <a class="mdl-button mdl-js-button mdl-button--icon twitter" href = "https://twitter.com/intent/tweet?status=<?php echo RUTA_IMG.'2.jpg' ?>" data-size = "large"><i class="fa fa-twitter"></i></a>
+                        <a class="mdl-button mdl-js-button mdl-button--icon linkedin" href = "https://www.linkedin.com/shareArticle?mini=true&url=http://www.sap-latam.com/sap_business_one/Prueba&title=Publicación&source=SAP" data-size="large"><i class="fa fa-linkedin"></i></a>
+                        <a class="mdl-button mdl-js-button mdl-button--icon facebook" href = "http://facebook.com/sharer.php?u=http://www.sap-latam.com/sap_business_one/Prueba" data-size="large"><i class="fa fa-facebook"></i></a>
+                        <a class="mdl-button mdl-js-button mdl-button--icon google" href = "https://plus.google.com/share?url=http://www.sap-latam.com/sap_business_one/Prueba" data-size="large"><i class="fa fa-google-plus"></i></a>
                     </div>
                 </div>
                 <div class="col-md-3">
