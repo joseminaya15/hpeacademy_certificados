6e7b24b78b0ad3677a412a53a7acdb374953d4c7
joseminaya15 <jose.minayac15@gmail.com>
2018-06-26 15:13:59 -0500

    imagenes

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/hpeacademy_certificados/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Pdf2.php
+++ application/controllers/Pdf2.php
@@ -14,7 +14,30 @@ class Pdf2 extends CI_Controller {
 
 	public function index(){
 		$html2pdf = new Html2Pdf();
-		$html2pdf->writeHTML('<h1>Hola mundo</h1>');
+		$html2pdf->writeHTML('<html>
+								<head>
+									<style>
+										.js-fondo{
+											background: url(http://www.marketinghpe.com/microsite/ecb/public/img/fondo/fondo-home.jpg) no-repeat center center;
+										    position: absolute;
+										    top: 0;
+										    left: 0;
+										    right: 0;
+										    bottom: 0;
+											background-size: cover;
+											height: 100vh;
+											z-index: 10;
+										}
+											h1{color: red;}
+										}
+									</style>
+								</head>
+								<body>
+									<div class="js-fondo"></div>
+									<h1>Hola mundo</h1>
+									<img src="http://www.marketinghpe.com/microsite/ecb/public/img/fondo/fondo-home.jpg"/>
+								</body>
+							  </html>');
 		$html2pdf->output();
 	}
 }
