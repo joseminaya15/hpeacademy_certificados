3d18c187a69df9bbf086f24763a14d14e0113d8f
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-06-26 15:21:53 -0500

    cambios

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hpeacademy_certificados/';
+$config['base_url'] = 'http://localhost:8080/hpeacademy_certificados/';
 
 /*
 |--------------------------------------------------------------------------
